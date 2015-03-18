465ccd0af0c2cfd25ef8c56c98217de48eea9160
Kevin Prouvot <kevin.prouvot@gmail.com>
2015-03-18 18:28:34 +0000

    Implementing CreditCard model

--- src/java/com/bcsg/csvmanager/models/CreditCard.java
+++ src/java/com/bcsg/csvmanager/models/CreditCard.java
@@ -1,5 +1,32 @@
 package java.com.bcsg.csvmanager.models;
 
 public class CreditCard {
-
+	
+	private String number;
+	private String bank;
+	private String expiryDate;
+	
+	public String getNumber() {
+		return number;
+	}
+	
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public String getBank() {
+		return bank;
+	}
+	
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
 }
