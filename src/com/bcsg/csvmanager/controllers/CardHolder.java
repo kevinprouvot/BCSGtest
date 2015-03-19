415203e52073172e646d771a2d651a2c35fd96fd
Kevin Prouvot <kevin.prouvot@gmail.com>
2015-03-19 13:27:26 +0000

    added mask method from HSBC Canada

--- src/com/bcsg/csvmanager/controllers/CardHolder.java
+++ src/com/bcsg/csvmanager/controllers/CardHolder.java
@@ -11,6 +11,9 @@ public class CardHolder {
 	private List<CreditCard> creditCards;
 	private List<CreditCard> unknowBankcards;
 	
+	private final Character DIGIT_SEPARATOR = '-';
+	private final String MASKED_DIGIT = "x";
+	
 	public CardHolder(List<CreditCard> creditCards) {
 		this.creditCards = creditCards;
 		this.unknowBankcards = new ArrayList<CreditCard>();
@@ -47,6 +50,46 @@ public class CardHolder {
 	}
 	
 	private void maskCardNumber(CreditCard creditCard) throws BankUnknownException{
+		switch(creditCard.getBank()) {
+			case HSBC_CANADA :
+				applyHSBCMask(creditCard);
+			break;
+			case ROYAL_BANK_OF_CANADA:
+				applyRoyalBankOfCanadaMask(creditCard);
+			break;
+			case AMERICAN_EXPRESS:
+				applyAmericanExpressMask(creditCard);
+			break;
+			default:
+				throw new BankUnknownException();
+		}
+	}
+	
+	// 2 firsts digits
+	private void applyHSBCMask(CreditCard creditCard) {		
+		String cardNumber = creditCard.getNumber();
+		StringBuffer maskedCardNumber = new StringBuffer();
+		
+		for (int index = 0 ; index < 2 ; index++) {
+			maskedCardNumber.append(cardNumber.charAt(index));
+		}
+		
+		for (int index = 2 ; index < cardNumber.length() ; index ++) {
+			if (cardNumber.charAt(index) != DIGIT_SEPARATOR) {
+				maskedCardNumber.append(MASKED_DIGIT);
+			}
+		}
+		
+		creditCard.setNumber(maskedCardNumber.toString());
+	}
+	
+	// 4 firsts digits
+	private void applyRoyalBankOfCanadaMask(CreditCard creditCard) {
+		
+	}
+	
+	// 3 lasts digits
+	private void applyAmericanExpressMask(CreditCard creditCard) {
 		
 	}
 	
