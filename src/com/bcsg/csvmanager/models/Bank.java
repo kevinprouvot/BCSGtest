6350b97571aaf852d033d93ec7fd4c45c1b75559
Kevin Prouvot <kevin.prouvot@gmail.com>
2015-03-19 12:06:58 +0000

    Added Bank Enum

---
+++ src/com/bcsg/csvmanager/models/Bank.java
@@ -0,0 +1,17 @@
+package com.bcsg.csvmanager.models;
+
+public enum Bank {
+	HSBC_CANADA("HSBC Canada"),
+	ROYAL_BANK_OF_CANADA("Royal Bank of  Canada"),
+	AMERICAN_EXPRESS("American Express");
+	
+	private String name = "";
+	
+	Bank(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+}
